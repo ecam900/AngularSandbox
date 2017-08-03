6fc152d5a9bc8f3c99deed595157c48cdcd8f19c
Eric Crescioni <ecam900@gmail.com>
2017-08-02 22:48:08 -0400

    ternary ngif operation

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:`<h1>Hello World</h1>`
+    template:`
+        <h1>Hello 
+        <!--
+            <span *ngIf="showName">{{name}}</span>
+            <span *ngIf="!showName">World</span>
+            -->
+            <span *ngIf="showName; else noName">{{name}}</span>
+            <ng-template #noName>World</ng-template>
+
+            <p>Hello {{ showName ? name : 'World' }}</p>
+            
+        </h1>
+        
+        `
 })
 
 export class SandboxComponent{
-    
+    name:string = 'Eric';
+    showName:boolean = false;
 }
