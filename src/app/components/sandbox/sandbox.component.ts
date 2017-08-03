3a71a019232876ab356608e37b26e1de32250286
Eric Crescioni <ecam900@gmail.com>
2017-08-02 22:38:58 -0400

    reset sandbox

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,48 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:`
-        <h1>Hello World</h1>
-        {{people}}
-
-        <ul>
-            <li *ngFor="let person of people">
-                {{ person }}
-            </li>
-        </ul>
-        <ul>
-            <li *ngFor="let person of people; let i = index">
-                {{i+1}} -->{{ person }}
-            </li>
-        </ul>
-
-        <ul>
-            <li *ngFor="let person of people2">
-                {{ person.firstName }} {{ person.lastName }}
-            </li>
-        </ul>
-    `
+    template:`<h1>Hello World</h1>`
 })
 
 export class SandboxComponent{
-    people = ['Rick', 'Morty', 'Jose', 'Carl', 'Shakwanda'];
-
-    people2 = [
-        {
-            firstName:'Rick',
-            lastName: 'Grimes'
-        },
-                {
-            firstName:'Carl',
-            lastName: 'Grimes'
-        },
-                {
-            firstName:'Poop',
-            lastName: 'Shatoonski'
-        }
-    ]
-
-    constructor(){
-        this.people[2] = 'Carol';
-    }
+    
 }
