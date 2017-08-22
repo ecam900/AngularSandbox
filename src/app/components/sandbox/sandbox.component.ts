013e6f607bd1e4957f262592653281b6a96ebde1
Eric Crescioni <ecam900@gmail.com>
2017-08-21 21:13:55 -0400

    Initial commit, sandbox time!

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,23 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:`
-        <h1>Hello 
-        <!--
-            <span *ngIf="showName">{{name}}</span>
-            <span *ngIf="!showName">World</span>
-            -->
-            <span *ngIf="showName; else noName">{{name}}</span>
-            <ng-template #noName>World</ng-template>
-
-            <p>Hello {{ showName ? name : 'World' }}</p>
-            
-        </h1>
-        
-        `
+    template:``
 })
 
 export class SandboxComponent{
-    name:string = 'Eric';
-    showName:boolean = false;
+
 }
