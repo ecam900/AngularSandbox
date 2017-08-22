2dba649fda3b067bf0156e7c58e0ccb94b324706
Eric Crescioni <ecam900@gmail.com>
2017-08-21 21:49:03 -0400

    Keyboard events are pretty cool to

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,22 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector:'sandbox',
     template:`
-        <h1>Changing properties with Events</h1>
-        <button (click)="changeValue()">Change Value!</button>
-        <div *ngIf="value">
-            <h1>{{ text }}</h1>
-        </div>
+         <h1>Checking out keyboard events:</h1>
+         <input type="text" (keyup)="fireEvent($event)">
     `
 })
 
 export class SandboxComponent{
-    
-    text:string = 'Hello!';
-    value:boolean = true;
-    
-    changeValue(){
-        //this.value = false;
-        this.value = !this.value;
-        //The above just toggles it :)
+    fireEvent(e){
+        console.log(e.type);
     }
 }
