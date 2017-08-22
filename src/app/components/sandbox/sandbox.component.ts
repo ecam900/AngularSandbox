88118ea7015e2045a3edbb694deaa157d647f3b5
Eric Crescioni <ecam900@gmail.com>
2017-08-21 21:20:46 -0400

    The gist is... pipes = filters.

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:``
+    template:`
+        <p> My birthday is {{ birthday | date }}</p>
+        <p> My birthday is {{ birthday | date:"MM-dd-yyyy" }}</p>
+        <hr/>
+        <p> My birthday year is is {{ birthday | date:"yyyy" }}</p>
+    `
+
+    /* Pipes are like filters. They make things look way better. They can also take parameters; example above */
 })
 
 export class SandboxComponent{
-
+    birthday = new Date(1990, 10, 4);
 }
