456e76a23d3401c005afeaf1775f3eebd47562f1
Eric Crescioni <ecam900@gmail.com>
2017-08-28 16:02:21 -0400

    2 way data binding and ngModel

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,14 +3,27 @@ import { Component } from '@angular/core';
 @Component({
     selector:'sandbox',
     template:`
-         <h1>Checking out keyboard events:</h1>
-         <input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
-         <input type="text" (keydown)="fireEvent($event)" placeholder="keydown event">
+        <h1>Hello world</h1>
+        <div class="container">
+            <form>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" [(ngModel)]="name" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                <label>Age</label>
+                <input type="number" [(ngModel)]="age" name="age" class="form-control">
+                </div>
+                <input type="submit" value="Submit" class="btn btn-success">
+            </form>
+
+            <h4>Name: {{ name }}</h4>
+            <h4>Age: {{ age }}</h4>
+        </div>
     `
 })
 
 export class SandboxComponent{
-    fireEvent(e){
-        console.log(e.type);
-    }
+    name:string = '';
+    age:number  = 10;
 }
