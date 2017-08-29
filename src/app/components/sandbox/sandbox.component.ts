95e94f763cc79377ead576c8ff1f6831c6266f1c
Eric Crescioni <ecam900@gmail.com>
2017-08-29 12:48:55 -0400

    Playing with post requests

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -5,6 +5,18 @@ import { DataService } from '../../services/data.service';
     selector:'sandbox',
     template:`
     <h1>Hello world</h1>
+    <form (submit)="onSubmit()">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" [(ngModel)]="user.name" name="name">
+            <label>Phone</label>
+            <input type="text" class="form-control" [(ngModel)]="user.phone" name="phone">
+            <label>Email</label>
+            <input type="text" class="form-control" [(ngModel)]="user.email" name="email">
+        </div>
+        <input type="submit" class="btn btn-success" value="Submit">
+    </form>
+    <hr>
     <div *ngFor="let user of users">
         <div class="well">
             <ul class="list-group">
@@ -19,12 +31,24 @@ import { DataService } from '../../services/data.service';
 
 export class SandboxComponent{
     users:any[];
+    user = {
+        name:'',
+        email:'',
+        phone:''
+    }
 
     constructor(public dataService:DataService)
     {
         this.dataService.getUsers().subscribe(users => {
             //console.log(users);
             this.users = users;
+        });
+    }
+
+    onSubmit(){
+        this.dataService.addUser(this.user).subscribe(user => {
+            console.log(user);
+            this.users.unshift(user);
         });
     }
 }
